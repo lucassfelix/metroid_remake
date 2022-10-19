d15aa8ea1cba2c45b8e8784cb490cd36dee84891
Lucas Salaverry Félix <lucas.salaverry@edu.pucrs.br>
2022-10-18 22:00:13 -0300

    Zoomer Debug

--- Scripts/ZoomerBehaviour.gd
+++ Scripts/ZoomerBehaviour.gd
@@ -4,11 +4,15 @@ extends Enemy
 onready var _frontRaycast = $CheckGroundFront
 onready var _backRaycast = $CheckGroundBack
 
+
 func _ready():
 	pass
 	
 func _process(delta):
 	._process(delta);
+	
+func _physics_process(delta):
+	._physics_process(delta)
 	_check_raycasts()
 	
 func _movement(delta : float):
@@ -20,21 +24,28 @@ func _check_raycasts():
 		_spin_right()
 
 func _spin_left():
+	print("Girou para a esquerda.")		
 	var angle := -90
 	var new_x : float = direction.x * cos(deg2rad(angle)) - direction.y * sin(deg2rad(angle))
 	var new_y : float = direction.x * sin(deg2rad(angle)) + direction.y * cos(deg2rad(angle))
 	direction = Vector2(new_x,new_y)
-	rotation_degrees += angle
+	global_rotation_degrees += angle
 
 func _spin_right():
+	print("Girou para a direita.")	
 	var angle := 90
 	var new_x : float = direction.x * cos(deg2rad(angle)) - direction.y * sin(deg2rad(angle))
 	var new_y : float = direction.x * sin(deg2rad(angle)) + direction.y * cos(deg2rad(angle))
 	direction = Vector2(new_x,new_y)
-	rotation_degrees += angle
+	get_transform().rotated(deg2rad(angle))
 
 
 func _on_collision(area : Area2D):
 	._on_collision(area);
 	if area.collision_layer == 4:
+		_spin_left()
+		
+func _on_collision_body(node : Node):
+	._on_collision_body(node);
+	if node.collision_layer == 4:
 		_spin_left()
